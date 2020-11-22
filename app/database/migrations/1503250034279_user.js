3669ceed93c4052a0f961931d4bed2bcd4ebc669
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-11-21 21:42:25 -0300

    1503250034279_user.js

--- app/database/migrations/1503250034279_user.js
+++ app/database/migrations/1503250034279_user.js
@@ -4,19 +4,20 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
-  }
+    up() {
+        this.create('users', (table) => {
+            table.increments()
+            table.string('name', 80).notNullable()
+            table.string('surname', 200).notNullable()
+            table.string('email', 254).notNullable().unique()
+            table.string('password', 60).notNullable()
+            table.timestamps()
+        })
+    }
 
-  down () {
-    this.drop('users')
-  }
+    down() {
+        this.drop('users')
+    }
 }
 
 module.exports = UserSchema
