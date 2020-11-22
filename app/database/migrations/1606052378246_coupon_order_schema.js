a2f15e17e3c5dde90721dc5cdce151bf977db3c3
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-11-22 10:46:59 -0300

    1606052378246_coupon_order_schema.js

---
+++ app/database/migrations/1606052378246_coupon_order_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponOrderSchema extends Schema {
+    up() {
+        this.create('coupon_orders', (table) => {
+            table.increments()
+            table.integer('coupon_id').unsigned()
+            table.integer('order_id').unsigned()
+            table.decimal('discount', 12, 2).defaultTo(0.0)
+            table.timestamps()
+            table.foreign('coupon_id').references('id').inTable('coupons').inDelete('cascade')
+            table.foreign('order_id').references('id').inTable('orders').inDelete('cascade')
+        })
+    }
+
+    down() {
+        this.drop('coupon_orders')
+    }
+}
+
+module.exports = CouponOrderSchema
