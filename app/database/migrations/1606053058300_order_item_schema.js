06b9ab6758b232850ef409efedc3876040dd5028
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-11-22 11:50:37 -0300

    1606053058300_order_item_schema.js

--- app/database/migrations/1606053058300_order_item_schema.js
+++ app/database/migrations/1606053058300_order_item_schema.js
@@ -8,13 +8,12 @@ class OrderItemSchema extends Schema {
         this.create('order_items', (table) => {
             table.increments()
             table.integer('product_id').unsigned()
-            table.integer('quantity');
-            unsigned()
+            table.integer('quantity').unsigned()
             table.decimal('subtotal', 12, 2)
             table.integer('order_id').unsigned()
             table.timestamps()
-            table.foreign('product_id').references('id').inTable('products').inDelete('cascade')
-            table.foreign('order_id').references('id').inTable('orders').inDelete('cascade')
+            table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+            table.foreign('order_id').references('id').inTable('orders').onDelete('cascade')
         })
     }
 
